1e6d9edf267513eb98e86ec1cf865a86c8bffde7
Rahul Saha <74053610+exusnraul@users.noreply.github.com>
2022-06-03 23:22:43 +0530

    a

--- anagrams.py
+++ anagrams.py
@@ -2,3 +2,38 @@ data = ['eat', 'ate', 'tea', 'ant', 'tan',
         'bat', 'adobe', 'abode', 'listen', 'silent']
 
 
+data = ['eat', 'ate', 'tea', 'ant', 'tan',
+        'bat', 'adobe', 'abode', 'listen', 'silent']
+ 
+ 
+def createAnagramKey(string):
+    key = ''
+    for ch in sorted(string):
+        key += ch
+    print(key)
+    return str(key)
+ 
+ 
+def groupAnagramWords(data):
+    group = dict()
+    for ele in data:
+        if group.get(createAnagramKey(ele)) == None:
+            group[createAnagramKey(ele)] = [ele]
+        else:
+            group[createAnagramKey(ele)].append(ele)
+    print(group)
+    return group
+ 
+ 
+anagram_grouped = groupAnagramWords(data)
+ 
+# Anagram in dictonry format
+print('In dictonry format')
+print(anagram_grouped)
+ 
+anagram_grouped_list = list()
+ 
+for k, v in anagram_grouped.items():
+    anagram_grouped_list.append(v)
+print('In list format')
+print(anagram_grouped_list)
