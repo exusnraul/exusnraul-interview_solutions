5cbf9d899869f730c382a0b1fa7a6c581e7ff272
Rahul Saha <74053610+exusnraul@users.noreply.github.com>
2022-06-20 22:53:04 +0530

    CGI Final round Data Analytics

--- new_interview.py
+++ new_interview.py
@@ -902,3 +902,159 @@ def func1(n):
         op.append(i**2)
 
 func1(10000)
+
+'''
+CGI- Final Client Round -- Data analytics
+RDBMS questions maily with join 
+'''
+'''
+table : employee
+id|name 
+100|praveen 
+200|naveen 
+300|kiran 
+400|Kishta
+
+table : dept 
+id|dept 
+300|A 
+400|B 
+500|A 
+600|B 
+700|C
+question from full outer join and Right Join
+'''
+
+{ "distance": { "text": "227 mi", "value": 365468 }, "duration": { "text": "3 hours 54 mins", "value": 14064 }, "status": "OK" }
+
+data={
+  "destination_addresses": [
+    "Washington, DC, USA",
+    "Philadelphia, PA, USA",
+    "Santa Barbara, CA, USA",
+    "Miami, FL, USA",
+    "Austin, TX, USA",
+    "Napa County, CA, USA"
+  ],
+  "origin_addresses": [
+    "New York, NY, USA"
+  ],
+  "rows": [{
+    "elements": [{
+        "distance": {
+          "text": "227 mi",
+          "value": 365468
+        },
+        "duration": {
+          "text": "3 hours 54 mins",
+          "value": 14064
+        },
+        "status": "OK"
+      },
+      {
+        "distance": {
+          "text": "94.6 mi",
+          "value": 152193
+        },
+        "duration": {
+          "text": "1 hour 44 mins",
+          "value": 6227
+        },
+        "status": "OK"
+      },
+      {
+        "distance": {
+          "text": "2,878 mi",
+          "value": 4632197
+        },
+        "duration": {
+          "text": "1 day 18 hours",
+          "value": 151772
+        },
+        "status": "OK"
+      },
+      {
+        "distance": {
+          "text": "1,286 mi",
+          "value": 2069031
+        },
+        "duration": {
+          "text": "18 hours 43 mins",
+          "value": 67405
+        },
+        "status": "OK"
+      },
+      {
+        "distance": {
+          "text": "1,742 mi",
+          "value": 2802972
+        },
+        "duration": {
+          "text": "1 day 2 hours",
+          "value": 93070
+        },
+        "status": "OK"
+      },
+      {
+        "distance": {
+          "text": "2,871 mi",
+          "value": 4620514
+        },
+        "duration": {
+          "text": "1 day 18 hours",
+          "value": 152913
+        },
+        "status": "OK"
+      }
+    ]
+  }],
+  "status": "OK"
+}
+
+new_data=data['rows'][0]['elements']
+# print(new_data)
+op=[]
+
+
+# for items in new_data:
+#     temp_=[]
+#     fil_dt=''
+#     temp_1=items['distance']
+#     temp_2=items['duration']
+#     temp_.append(temp_1.values())
+#     temp_.append(temp_2.values())
+#     temp_.append(items['status'])
+#     # fil_dt='|'.join(temp_)
+#     # op.append(fil_dt)
+# print(temp_)
+# print(op)
+#Not working code
+def json_extract(obj, key):
+    """Recursively fetch values from nested JSON."""
+    arr = []
+
+    def extract(obj, arr, key):
+        """Recursively search for values of key in JSON tree."""
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                if isinstance(v, (dict, list)):
+                    extract(v, arr, key)
+                elif k == key:
+                    arr.append(v)
+        elif isinstance(obj, list):
+            for item in obj:
+                extract(item, arr, key)
+        return arr
+
+    values = extract(obj, arr, key)
+    return values
+
+print(json_extract(data,'text'))
+'''id|name|manager_id 
+1|praveen|2 
+2|Murali|3 
+3|Ketav|4 
+4|Kishta|null 
+6|Naveen|3
+Show items with who are managers
+select t1.name from table t1 self join table t2 where t1.id=t2.manager_id '''
